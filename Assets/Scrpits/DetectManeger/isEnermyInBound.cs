a01dacfdfa3ceadde97802f33f239eda6f165b63
verpivia-1 <cyrr0070088@gmail.com>
2026-01-22 17:50:36 +0900

    UI.
    PlayManu Ui update

---
+++ Assets/Scrpits/DetectManeger/isEnermyInBound.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class isEnermyInBound : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
